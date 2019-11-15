88bd7b3673271da2058fdb4aaf4a37ced4a5c802
varadjoshi89 <joshi.vara@husky.neu.edu>
2019-11-15 00:44:13 -0500

    function

--- webapp/src/main/java/com/csye6225/EventLog.java
+++ webapp/src/main/java/com/csye6225/EventLog.java
@@ -27,20 +27,98 @@ public class EventLog implements RequestHandler<SNSEvent, Object> {
     static final String subject = "Recipe Links";
     static String htmlBody;
     private static String textBody;
-    private String body="";
+    private String body = "";
     static String token;
     static String username;
     List<String> myList;
 
     public Object handleRequest(SNSEvent request, Context context) {
+        String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
 
-        //creating a DynamoDB Client
-        private void initDynamoDbClient() {
-            AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
-                    .withRegion(region)
+        String Domain = "//csye6225-fall2019";
+        context.getLogger().log("domain" + Domain);
+        from = "noreply@" + Domain;
 
-                    .build();
-            dynamoDb = new DynamoDB(client);
+        //Creating ttl
+        context.getLogger().log("Invocation started: " + timeStamp);
+        long now = Calendar.getInstance().getTimeInMillis() / 1000; // unix time
+        long ttl = 60 * 30; // ttl set to 15 min
+        long totalttl = ttl + now;
+
+        try {
+            //Function Excecution for sending the email
+            myList = new ArrayList<String>(Arrays.asList(request.getRecords().get(0).getSNS().getMessage().split(",")));
+            username = myList.get(myList.size() - 1);
+            myList.remove(myList.size() - 1);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+        token = UUID.randomUUID().toString();
+        context.getLogger().log("Invocation completed: " + timeStamp);
+
+        try {
+            initDynamoDbClient();
+            long ttlDbValue = 0;
+            context.getLogger().log("hey1 " + timeStamp);
+            if (this.dynamoDb.getTable(tableName).describe().getItemCount() != 0) {
+                Item item = this.dynamoDb.getTable(tableName).getItem("id", username);
+                context.getLogger().log("hey2 " + timeStamp);
+                ttlDbValue = item.getLong("ttl");
+
+                if (ttlDbValue <= now && ttlDbValue != 0) {
+
+                    context.getLogger().log("Checking for valid ttl");
+                    context.getLogger().log("ttl expired, creating new token and sending email");
+                    this.dynamoDb.getTable(tableName)
+                            .putItem(
+                                    new PutItemSpec().withItem(new Item()
+                                            .withString("id", username)
+                                            .withString("token", token)
+                                            .withLong("ttl", totalttl)));
+                    
+                    for (String recipe : myList) {
+                        body = body + "<p>" + recipe + "</p>";
+                    }
+                    htmlBody = "<h2>Email sent from Amazon SES</h2>"
+                            + body;
+                    context.getLogger().log("This is HTML body: " + htmlBody);
+
+
+                    //Sending email using Amazon SES client
+                    AmazonSimpleEmailService clients = AmazonSimpleEmailServiceClientBuilder.standard()
+                            .withRegion(region).build();
+                    SendEmailRequest emailRequest = new SendEmailRequest()
+                            .withDestination(
+                                    new Destination().withToAddresses(username))
+                            .withMessage(new Message()
+                                    .withBody(new Body()
+                                            .withHtml(new Content()
+                                                    .withCharset("UTF-8").withData(htmlBody))
+                                            .withText(new Content()
+                                                    .withCharset("UTF-8").withData(textBody)))
+                                    .withSubject(new Content()
+                                            .withCharset("UTF-8").withData(subject)))
+                            .withSource(from);
+                    clients.sendEmail(emailRequest);
+                    context.getLogger().log("Email sent successfully to email id: " + username);
+
+                } else {
+                    context.getLogger().log("ttl is not expired. New request is not processed for the user: " + username);
+                }
+            }
+        } catch (Exception ex) {
+            context.getLogger().log("Email was not sent. Error message: " + ex.getMessage());
+        }
+        return null;
+    }
+
+    //creating a DynamoDB Client
+    private void initDynamoDbClient() {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withRegion(region)
+
+                .build();
+        dynamoDb = new DynamoDB(client);
     }
 }
