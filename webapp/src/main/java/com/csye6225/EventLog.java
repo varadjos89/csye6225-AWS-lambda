cb32a53ed60368c15e35a2662bdc76c32d239799
varadjoshi89 <joshi.vara@husky.neu.edu>
2019-11-15 00:35:48 -0500

    webapp update

--- webapp/src/main/java/com/csye6225/EventLog.java
+++ webapp/src/main/java/com/csye6225/EventLog.java
@@ -19,5 +19,20 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 
-public class EventLog {
+public class EventLog implements RequestHandler<SNSEvent, Object> {
+    static DynamoDB dynamoDb;
+    private String tableName = "csye6225_DynamoDBDevTwothree";
+    private Regions region = Regions.US_EAST_1;
+    public String from = "";
+    static final String subject = "Recipe Links";
+    static String htmlBody;
+    private static String textBody;
+    private String body="";
+    static String token;
+    static String username;
+    List<String> myList;
+
+    public Object handleRequest(SNSEvent request, Context context) {
+        
+    }
 }
